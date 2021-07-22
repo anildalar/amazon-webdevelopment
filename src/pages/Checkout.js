678289ac31345540bb256044e33e73c3a749c203
Antim167 <singhantim505@gmail.com>
2021-07-22 09:41:22 +0530

    checkout page done

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -8,11 +8,31 @@ import Layout from '../components/Layout'
 **/
 
 class Checkout extends Component {
+  //1 . Property
   state = {}
+  
+  sty = `
+  .a_checkout_main{
+        height:1000px;
+        border:1px dashed red;
+      }
+      
+  `;
+
+  //2. Construc
+
+
+  //3. Method
   render() {
     return (
       <Layout>
+        <style>
+          { this.sty }
+        </style>
         <h1>This is Checkout Pages</h1>
+        <div className="a_checkout_main">
+          
+        </div>
       </Layout>
     )
   }
