43bbb7c3c61b83c990a439e01f248dd6d7597f30
Mujammil Shaikh <mujammilshaikh4091@gmail.com>
2023-02-05 16:38:33 +0530

    Added some paragraph

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,6 +3,9 @@ export default function  Cart()
 return(
     <>
     <h1>Cart page</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nostrum dolores et vel quasi hic laudantium consequatur voluptatibus minus tenetur reprehenderit iste ullam incidunt magni quae ad, doloremque itaque commodi?
+    </p>
     </>
 )
 }
